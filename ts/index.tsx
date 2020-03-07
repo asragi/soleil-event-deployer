e8622d5ae90ff7e708475fb2a06647303da05450
asRagi <ragi-justaway@hotmail.co.jp>
2020-03-08 00:14:37 +0900

    end f day5

--- ts/index.tsx
+++ ts/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-import UserForm from './conponents/UserForm';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './conponents/FoundationStyles';
+import TaskList from './conponents/TaskList';
 import Store from './Store';
 
 const container = document.getElementById('contents');
 
 ReactDom.render(
-    <Provider store={Store}>
-        <UserForm />
-    </Provider>,
+    <div>
+        <Provider store={Store}>
+            <TaskList />
+        </Provider>
+        <ThemeProvider theme={{}}>
+            <GlobalStyle theme="" />
+        </ThemeProvider>
+    </div>,
     container,
 );
