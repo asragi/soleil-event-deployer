f001e5e938805fcf47d5fad3e6f8512d06382a58
asRagi <ragi-justaway@hotmail.co.jp>
2020-03-11 15:15:09 +0900

    update til TaskActionCreator

--- ts/actions/TaskActionCreators.ts
+++ ts/actions/TaskActionCreators.ts
@@ -1,5 +1,8 @@
+import { Dispatch, Store } from "redux";
 import Moment from 'moment';
+import { IState } from '../IStore';
 import { ITask } from '../states/ITask';
+import { loadTask, saveState } from '../utils/TaskFileIF';
 import { 
     ADD_TASK,
     DELETE_TASK,
@@ -7,62 +10,80 @@ import {
     IDeleteAction,
     IShowTaskAction,
     IToggleCompleteAction,
+    IToggleShownSpinnerAction,
     SHOW_TASKS,
     TOGGLE_COMPLETE_TASK,
+    TOGGLE_SHOWN_SPINNER,
 } from "./TaskActions";
 
 export const createShowTaskAction = (tasks: ITask[]): IShowTaskAction => {
-    // dummy
-    const dummyTasks: ITask[] = [
-        {
-            complete: false,
-            deadline: Moment().add(1, 'day').toDate(),
-            id: '0',
-            taskName: 'task01',
-        },
-        {
-            complete: true,
-            deadline: Moment().add(1, 'day').toDate(),
-            id: '1',
-            taskName: 'task02',
-        },
-        {
-            complete: false,
-            deadline: Moment().add(-1, 'day').toDate(),
-            id: '2',
-            taskName: 'task03',
-        },
-        {
-            complete: true,
-            deadline: Moment().add(-1, 'day').toDate(),
-            id: '3',
-            taskName: 'task04',
-        },
-    ];
     return {
-        tasks: dummyTasks,
+        tasks: tasks,
         type: SHOW_TASKS,
     };
 };
 
-export const createAddTaskAction = (taskName: string, deadline: Date): IAddTaskAction => {
+export const createAddTaskAction =
+    (taskName: string, deadline: Date, store: Store<IState>): IToggleShownSpinnerAction => {
+    (async () => {
+        const addAction: IAddTaskAction = {
+            deadline,
+            taskName,
+            type: ADD_TASK,
+        };
+        store.dispatch(addAction);
+        const taskList = store.getState().taskList;
+        await saveState(taskList.tasks);
+        store.dispatch<IToggleShownSpinnerAction>({
+            type: TOGGLE_SHOWN_SPINNER,
+        });
+    })();
     return {
-        deadline,
-        taskName,
-        type: ADD_TASK,
+        type: TOGGLE_SHOWN_SPINNER,
     };
 };
 
-export const createToggleCompleteAction = (taskId: string): IToggleCompleteAction => {
+export const createToggleCompleteAction =
+    (taskId: string, store: Store<IState>): IToggleShownSpinnerAction => {
+        (async () => {
+            store.dispatch<IToggleCompleteAction>( { taskId, type: TOGGLE_COMPLETE_TASK });
+            const taskList = store.getState().taskList;
+            await saveState(taskList.tasks);
+            store.dispatch<IToggleShownSpinnerAction>({
+                type: TOGGLE_SHOWN_SPINNER,
+            });
+        })();
     return {
-        taskId,
-        type: TOGGLE_COMPLETE_TASK,
+        type: TOGGLE_SHOWN_SPINNER,
     };
 };
 
-export const createDeleteTaskAction = (taskId: string): IDeleteAction => {
+export const createDeleteTaskAction =
+    (taskId: string, store: Store<IState>): IToggleShownSpinnerAction => {
+        (async () => {
+            store.dispatch<IDeleteAction>( { taskId, type: DELETE_TASK });
+            const taskList = store.getState().taskList;
+            await saveState(taskList.tasks);
+            store.dispatch<IToggleShownSpinnerAction>({
+                type: TOGGLE_SHOWN_SPINNER,
+            });
+        })();
     return {
-        taskId,
-        type: DELETE_TASK,
+        type: TOGGLE_SHOWN_SPINNER,
     };
 };
+
+export const createLoadTasksAction = (dispatch: Dispatch): IToggleShownSpinnerAction => {
+    let tasks: ITask[] = [];
+    loadTask().then((jsonData) => {
+        tasks = jsonData.data as ITask[];
+        dispatch(createShowTaskAction(tasks));
+        dispatch<IToggleShownSpinnerAction>({
+            type: TOGGLE_SHOWN_SPINNER,
+        });
+    });
+    // loadtask()は非同期のためこのreturnが先．
+    return {
+        type: TOGGLE_SHOWN_SPINNER,
+    }
+}
---
+++ ts/utils/TaskFileIF.ts
@@ -0,0 +1,43 @@
+import FsEx from "fs-extra";
+import OS from 'os';
+import Path from 'path';
+
+import { ITask } from '../states/ITask';
+
+const dataFilePath = Path.join(OS.homedir(), 'todo.json');
+
+export const loadTask = async () => {
+    const exist = await FsEx.pathExists(dataFilePath);
+    if (!exist) {
+        FsEx.ensureFileSync(dataFilePath);
+        await FsEx.writeJson(dataFilePath, { data: [] });
+    }
+    const jsonData = await FsEx.readJSON(dataFilePath, {
+        reviver: (key: string, value: any) => {
+            if (key === 'deadline') {
+                return new Date(value as number);
+            } else {
+                return value;
+            }
+        }
+    });
+    await setTimeoutPromise(1000);
+    return jsonData;
+};
+
+export const saveState = async (taskList: ITask[]) => {
+    await setTimeoutPromise(1000);
+    await FsEx.writeJSON(dataFilePath, { data: taskList }, {
+        replacer: (key: string, value: any) => {
+            if (key !== 'deadline') { return value; }
+            return new Date((value as string)).getTime();
+        },
+        spaces: 2,
+    });
+};
+
+const setTimeoutPromise = (count: number) => {
+    return new Promise((resolve) =>{
+        setTimeout(() => { resolve(); }, count);
+    });
+};
