80bd6ab8855ece4563462cddde4f4132ca89376f
asRagi <ragi-justaway@hotmail.co.jp>
2020-04-29 18:17:13 +0900

    update input change edit

--- ts/components/eventWindow/EventBaseEdit/InputChangeEdit.tsx
+++ ts/components/eventWindow/EventBaseEdit/InputChangeEdit.tsx
@@ -1,23 +1,8 @@
 import React from 'react';
 import Clone from 'clone';
-import Styled from 'styled-components';
-import Footer from '../../DecideButton';
-import { CenterWindow, Title, StyledHr } from '../../GeneralComponent';
-import { $NewEventWindow } from '../../../utils/DepthNum';
 import { IEventBase } from '../../../states/IEvent';
 import { FocusList } from '../../../utils/InputType';
-
-//#region style
-const WindowBack = Styled(CenterWindow(60, $NewEventWindow))`
-    display: flex;
-    flex-direction: column;
-`;
-const Container = Styled.div`
-    display: flex;
-    flex-direction: column;
-    padding: 10px;
-`;
-//#endregion
+import EditWindowParent from './EditWindow';
 
 interface IProps {
     onCancel: () => void;
@@ -39,30 +24,29 @@ class InputChangeEdit extends React.Component<IProps, LocalState> {
     }
 
     public render() {
+        const title = '入力先フラグの変更';
         const { onCancel } = this.props;
         return(
-            <WindowBack>
-                <Container>
-                    <Title>{'入力先フラグの変更'}</Title>
-                    <StyledHr />
-                    {'Content'}
+            <EditWindowParent
+                onCancel={onCancel}
+                onSubmit={this.onSubmit}
+                title={title}>
+                    {'Content: InputFocusTarget'}
                     {this.renderPullDownMenu(this.onChangeContent)}
-                </Container>
-                <Footer onCancel={onCancel} onSubmit={this.onSubmit}/>
-            </WindowBack>
+            </EditWindowParent>
         );
     }
 
     private renderPullDownMenu =
         (onChangeEvent: (e: React.ChangeEvent<HTMLSelectElement>) => void) => {
-        const options = FocusList.map(
-            s => <option key={s} value={s}>{s}</option>
-        );
-        return(
-            <select onChange={onChangeEvent}>
-                {options}
-            </select>
-        );
+            const options = FocusList.map(
+                s => <option key={s} value={s}>{s}</option>
+            );
+            return(
+                <select onChange={onChangeEvent}>
+                    {options}
+                </select>
+            );
     }
 
     private onChangeContent = (e: React.ChangeEvent<HTMLSelectElement>) => {
